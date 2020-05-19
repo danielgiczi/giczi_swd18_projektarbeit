c3ae731b8af3a36348451b49ada1ac48a925b7ee
Daniel Giczi <danielgiczi@edu.fh-joanneum.at>
2020-05-19 23:17:15 +0200

    Test

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const port = 3000;
 const path = require("path")
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
+
 const app = express()
 
 if (process.env.NODE_ENV === "development") { 
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+
     const config = require('./webpack.dev.config.js');
     const compiler = webpack(config);
 
@@ -19,8 +20,6 @@ if (process.env.NODE_ENV === "development") {
     }));
 }
 
-app.set('port', process.env.PORT || port)
-port = process.env.PORT || port
 app.use(bodyParser.json())
 
 var distDir = path.join(__dirname, "dist")
@@ -37,6 +36,7 @@ if (process.env.NODE_ENV != "development") {
     })
 }
 
-app.listen(port, function(){
-    console.log(`Listening on Port ${port}...`)
+const PORT = process.env.PORT ||3000
+app.listen(PORT, function(){
+    console.log(`Listening on Port ${PORT}...`)
 })
